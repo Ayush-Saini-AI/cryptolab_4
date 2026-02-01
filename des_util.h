d97dd90b698dafbe161eca2f6cbe212521f34a0d
Ayush Saini <1ayushsaini@gmail.com>
2026-02-01 09:59:18 +0530

    s des done

--- des_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef DES_UTIL_H
-#define DES_UTIL_H
-
-#include <stddef.h>
-
-void des_encrypt(unsigned char *data, size_t length, const unsigned char key[8]);
-void des_decrypt(unsigned char *data, size_t length, const unsigned char key[8]);
-
-#endif
